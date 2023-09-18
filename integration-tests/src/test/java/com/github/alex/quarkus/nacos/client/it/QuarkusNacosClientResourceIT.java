6b6132fa46f58eb2af98f11a61354bac6913341a
邹亚男 <zouyanan@eccom.com.cn>
2023-09-18 14:20:30 +0800

    移除继承测试代码

--- integration-tests/src/test/java/com/github/alex/quarkus/nacos/client/it/QuarkusNacosClientResourceIT.java
+++
@@ -1,7 +0,0 @@
-package com.github.alex.quarkus.nacos.client.it;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class QuarkusNacosClientResourceIT extends QuarkusNacosClientResourceTest {
-}
