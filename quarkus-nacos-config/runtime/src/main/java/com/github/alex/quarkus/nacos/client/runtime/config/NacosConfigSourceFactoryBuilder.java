577b8752cbe9bc827d4e2b6a62c954f323cdb40b
邹亚男 <zouyanan@eccom.com.cn>
2023-09-19 08:59:08 +0800

    调整目录

--- quarkus-nacos-config/runtime/src/main/java/com/github/alex/quarkus/nacos/client/runtime/config/NacosConfigSourceFactoryBuilder.java
+++ quarkus-nacos-config/runtime/src/main/java/com/github/alex/quarkus/nacos/client/runtime/config/NacosConfigSourceFactoryBuilder.java
@@ -6,7 +6,6 @@ import io.smallrye.config.SmallRyeConfigBuilder;
 public class NacosConfigSourceFactoryBuilder implements ConfigBuilder {
     @Override
     public SmallRyeConfigBuilder configBuilder(final SmallRyeConfigBuilder builder) {
-        return builder.withSources(new NacosConfigSourceFactory())
-                .withInterceptors(new LoggingConfigSourceInterceptor());
+        return builder.withSources(new NacosConfigSourceFactory());
     }
 }
